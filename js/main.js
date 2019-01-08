c4bd43a64aaf31145650152d561ef8df6d222f7d
Ursula CJ <ursulacedillojohnson@gmail.com>
2019-01-08 00:17:43 -0600

    created first game logic

--- js/main.js
+++ js/main.js
@@ -1,10 +1,23 @@
-console.log("Up and running!");
-
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
-
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = []; 
+var cardOne = cards[3];
+cardsInPlay.push(cardOne);
 console.log("User flipped " + cardOne);
+var cardTwo = cards[2];
+cardsInPlay.push(cardTwo);
+console.log("User flipped " + cardTwo);
+
+if(cardsInPlay.length === 2 && cardsInPlay[0] === cardsInPlay[1]){
+// if there are two cardsInPlay and they're the same 
+alert("You found a match!");
+// say "match"
+}
+else {
+	alert("Sorry, try again.");
+// else say "pick again"
+// and reset deck	
+}
+
+
 
 
