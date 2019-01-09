e5be468e5078b58747f2b614900a051b93497d3a
Ursula CJ <ursulacedillojohnson@gmail.com>
2019-01-09 17:07:22 -0600

    created object-oriented game logic

--- js/main.js
+++ js/main.js
@@ -1,30 +1,65 @@
-var cards = ["queen", "queen", "king", "king"];
+var cards = [
+	{
+	rank: "queen", 
+	suit: "hearts", 
+	cardImage: "images/images/queen-of-hearts.png"
+    },
+	{
+	rank: "queen",
+	suit: "diamonds",
+	cardImage: "images/images/queen-of-diamonds.png" 
+	},
+	{
+	rank: "king",
+	suit: "hearts",
+	cardImage: "images/images/king-of-hearts.png"
+	},
+	{
+	rank:  "king",
+	suit: "diamonds",
+	cardImage: "images/images/king-of-diamonds.png"
+    }
+];
+
 var cardsInPlay = [];
 
-var checkForMatch = function(){
+var checkForMatch = function() {
 	if (cardsInPlay[0] === cardsInPlay[1]) {
 		alert("You found a match!");
-	}
+	} 
 	else {
 		alert("Sorry, try again.");
 	}
-};
+    };
 
 var flipCard = function(cardId) {
 if(cardsInPlay.length === 1) {
-	checkForMatch();
-console.log("User flipped " + cards[cardId]); 
-cardsInPlay.push(cards[cardId]);
+	//this is based around the idea that you have to have one flipped already to get two to match 
+  //you might have to change this to accomodate the return function to reset the cards
+  console.log("ready to match!");
+   console.log("User flipped " + cards[cardId].rank);
+      console.log(cards[cardId].cardImage);
+      console.log("flipped card suit: " + cards[cardId].suit);
+    cardsInPlay.push(cards[cardId].rank);
+    console.log("all flipped cards: " + cardsInPlay);
+    //comment the above line out when you're sure that everything works
+    checkForMatch();
 }
-else 
-	console.log("User flipped " + cards[cardId]);
-	cardsInPlay.push(cards[cardId]);
+else { 
+console.log("only one card flipped. flip one more!");
+  console.log("User flipped " + cards[cardId].rank);
+        console.log(cards[cardId].cardImage);
+        console.log("flipped card suit: " + cards[cardId].suit);
+  cardsInPlay.push(cards[cardId].rank);
+  console.log("all flipped cards: " + cardsInPlay);
+  //comment the above line out when you're sure that everything works
+}
 };
 
+flipCard(0);
 flipCard(2);
-console.log(cardsInPlay.length);
-flipCard(1);
-/* based on your tests, you'll probably have to change the 
-length to 1 so it shows that you just flipped the second*/
+
+
+
 
 
